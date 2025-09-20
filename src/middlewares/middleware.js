73a88150d518c5169575852f441910ce76f20e3a
shivanijaiswar13 <shivanijaiswar01@gmail.com>
2025-09-20 19:53:52 +0530

    fix: improve error handling and token validation in auth middleware

--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -13,8 +13,27 @@ const authMiddleware = async (req, res, next)=>{
                 message: "token not found, Unauthorized",
             });
         }
-        const isBlacklisted = await cacheClient.get(token)
+        const isBlacklisted = await cacheClient.get(token);
+        if(isBlacklisted){
+            return res.status(401).json({
+                message: "token blacklisted",
+            });
+        }
+        let decoded = jwt.verify(token, process.env.JWT_SECRET);
+        if(!decoded){
+            return res.status(401).json({
+                message: "Invalid token",
+            });
+
+        }
+
+        let user = await userModel.findById(decode.id);
+        req.user = user;
+        next();
+
     } catch (error) {
-        
+        console.log("error in middleware", error);
     }
 }
+
+module.exports = authMiddleware;
